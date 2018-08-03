c0939d8a173a929e00cf3b4c19cdd7317c54b909
Chelsea <cnoelle1091@gmail.com>
2018-08-02 18:46:49 -0700

    Finished express intro exercise

--- part1/server.js
+++ part1/server.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
+var path = require("path");
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
+});
+
+app.post("/create/:name", function(req, res){
+  res.json({
+    "id": 1,
+    "name": req.params.name
+  });
+});
+
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get("/verify/:age", function(req, res){
+  if(req.params.age >= 13){
+    res.sendStatus(200);
+  }
+  if(req.params.age < 13){
+    res.sendStatus(403);
+  }
 });
 
 app.use(function(req, res) {
--- part2/server.js
+++ part2/server.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
+var path = require("path");
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.post('/create/:name/:age', function(req, res) {
+  let jsData = retrieveData();
+
+  var newPerson = {
+    "name": req.params.name,
+    "age": req.params.age
+  }
+  jsData.push(newPerson);
+  fs.writeFileSync('./part2/storage.json', JSON.stringify(jsData));
+  res.json(newPerson);
+});
+
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname+'/storage.json'));
+});
+
+app.get("/:name", function(req, res){
+  let jsData = retrieveData();
+  for(let i=0; i<jsData.length; i++){
+    let person = jsData[i];
+    if(person.name === req.params.name){
+      res.json(person);
+      return;
+    }
+  }
+  res.sendStatus(400);
 });
 
 app.use(function(req, res) {
@@ -13,3 +39,9 @@ app.use(function(req, res) {
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
+
+function retrieveData(){
+  let rawData = fs.readFileSync("./part2/storage.json", "utf8");
+  let jsData = JSON.parse(rawData);
+  return jsData;
+}
